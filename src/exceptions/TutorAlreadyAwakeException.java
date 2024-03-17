ee13cafd3e4c003230e857f62cbb54bd61e07ec3
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2024-03-17 16:55:08 -0500

    Deleted Exceptions folder

--- src/exceptions/TutorAlreadyAwakeException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class TutorAlreadyAwakeException extends RuntimeException{
-    public TutorAlreadyAwakeException(){
-        super("Tutor is already awake!");
-    }
-}
